726a0a2ebdad1b8593477d392b5e640920c1f4d5
ricardo ortega <ricardo_oz5@hotmail.com>
2019-04-10 00:14:51 -0500

    mi segundo commit

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {} from '@angular/core/src/render3';
+
+@Component(
+
+    {
+        selector: 'app-body', 
+        templateUrl: './body.component.html'
+
+    }
+)
+
+export class BodyComponent{
+
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+import { template } from '@angular/core/src/render3';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'    
+    
+})
+
+export class HeaderComponent{
+
+}
